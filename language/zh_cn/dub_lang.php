f872fc81eb5d11030f77195c9102d802b640a93a
lq <liuqiang1981@gmail.com>
2014-04-11 15:00:59 +0800

    update

--- language/zh_cn/dub_lang.php
+++ language/zh_cn/dub_lang.php
@@ -1,17 +1,17 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
 * Name:  Ion Auth Lang - Chinese Simplified
-* 
+*
 * Author: Kain Liu
 * 		  Lkaihua@gmail.com
 *         @China
-* 
+*
 * Location: http://github.com/benedmunds/ion_auth/
-*          
+*
 * Created:  10.24.2011
-* 
+*
 * Description:  Simplified Chinese language file for Ion Auth messages and errors
-* 
+*
 */
 
 // Account Creation
@@ -21,31 +21,50 @@ $lang['ages_item']= array(
 	'16' => '少年(13-19)',
 	'24' => '青壮年(20-39)',
 	'50' => '中年(40-65)',
-	'70' => '老年(&gt;66)'		
+	'70' => '老年(&gt;66)'
 );
 
-$lang['gender_item']= array(	
-	'm' => '男',	
+$lang['gender_item']= array(
+	'm' => '男',
 	'f' => '女'
 );
 
 $lang['lang_item']= array(
-	'cn' => '普通话',	
+	'cn' => '普通话',
 	'en' => '英语'
 );
 
 $lang['project_type']= array(
-	'jb' => '竞配',	
+	'jb' => '竞配',
 	'st' => '试听',
 	'sp' => '速配',
 );
 
 $lang['project_status']= array(
-	'0' => '待审核',	
+	'0' => '待审核',
 	'1' => '进行中',
 	'2' => '已完成',
 );
 
+$lang['sample_type']= array(
+	'1'=>'广告',
+	'2'=>'专题',
+	'3'=>'纪录片',
+	'4'=>'预告片',
+	'5'=>'微电影',
+	'6'=>'电台/电视',
+	'7'=>'有声读物',
+	'8'=>'电话语音',
+	'9'=>'游戏电玩',
+);
+
+$lang['emotion_item']= array(
+'1'=>'浑厚/大气',
+'2'=>'温情/磁性',
+'3'=>'时尚/活力',
+'4'=>'甜美/稚嫩',
+'5'=>'温柔/知性'
+);
 
 /* End of file dub_lang.php */
 /* Location: ./system/language/chinesesimplified/db_lang.php */
